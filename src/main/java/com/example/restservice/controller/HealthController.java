0eae233806b63e4ed49853feea3af6eb9229c0c6
Okogu <49090711+Okogu@users.noreply.github.com>
2021-09-08 13:54:41 +0100

    moved

---
+++ src/main/java/com/example/restservice/controller/HealthController.java
@@ -0,0 +1,26 @@
+package com.example.restservice.controller;
+
+import com.example.restservice.dto.ApiResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+
+public class HealthController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public ApiResponse fetchProfiles() {
+        ApiResponse apiResponse = new ApiResponse();
+
+            apiResponse.setSuccess(true);
+            apiResponse.setMessage("Linqup backend is running");
+
+        return apiResponse;
+
+    }
+}
